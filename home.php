22d7214b17ccabac99379b41119e0dfe5fc0b85f
Pete <pgm987@gmail.com>
2013-03-12 16:12:34 +0000

    Corrected Spelling

--- home.php
+++ home.php
@@ -1,9 +1,9 @@
 <h2>About Me</h2>
 <img src="images/pages/profile.jpg" alt="Image of Pete" class="left" />
-<p>Hello, I'm Pete! I'm a computer scientist, currently studying in Aberystwyth UK.  I have a keen intrest in computer networks and programming.  I enjoy network security and server administration.</p>
+<p>Hello, I'm Pete! I'm a computer scientist, currently studying in Aberystwyth UK.  I have a keen interest in computer networks and programming.  I enjoy network security and server administration.</p>
 
 <p>Feel free to add me on any of the social networks above, whilst your here have a look at my <a href="index.php?p=blog">blog</a>, it might have 
-something intresting.  Also make sure to check out my <a href="index.php?p=cv">CV</a> and <a href="index.php?p=portfolio">Portfolio</a>.</p>
+something interesting.  Also make sure to check out my <a href="index.php?p=cv">CV</a> and <a href="index.php?p=portfolio">Portfolio</a>.</p>
 <p>If you would like to contact me my mobile number is: <b>07792 504 654</b> and my e-mail is <b>pgm987[@]gmail.com</b></p>
 
 <p>Thank you for looking, Pete</p>
--- portfolio.php
+++ portfolio.php
@@ -1,39 +1,39 @@
 			<h2>Portfolio</h2>
 			<p>Here is a collection of work that I have done in my spare time or for clients.  Topics range from web design, assignments and informational reports.</p>
-			<p>Please accept my appologies for any spelling or gramatical errors contained within the following documents. These are mainly here for historical puposes.</p>
+			<p>Please accept my apologies for any spelling or grammatical errors contained within the following documents. These are mainly here for historical purposes.</p>
 			
 			<h2>Programming</h2>
 			<p>This area will contain links to code and applications that I have written either for assignments or in my spare time.</p>
 			
 			<h3>Computer Science BSc Hon</h3>
-			<p><a href="files/portfolio/Quick_Sort_Optimization.pdf">Quick Sort Optimization</a> - This report details the results of quick sort optimization. Using diferent simple sorts when the data set reached a certain limit. Please see assignment <a href="files/portfolio/Quick_Sort_Optimization_Specification.pdf">specification</a></p>
+			<p><a href="files/portfolio/Quick_Sort_Optimization.pdf">Quick Sort Optimization</a> - This report details the results of quick sort optimization. Using different simple sorts when the data set reached a certain limit. Please see assignment <a href="files/portfolio/Quick_Sort_Optimization_Specification.pdf">specification</a></p>
 			
 			<h3>HND Computing</h3>
 			<p>This work was done a while ago 2008/09, and are on here for historical purposes</p>
 			<p><a href="files/portfolio/VB-Database.zip">Visual Basic Database</a> - This is a simple application written for second year event driven programming module.  It was written in Visual Studio 2008, the download contains the project file, as well as the compiled binary files for windows and an example database to play with.</p>  
-			<p>The program is a simplefied banking system.  It allows users to log into the system useing their card PIN.  It logs all transactions to a relational database, Microsoft Access, it also manages users failed login attempts and if nessesory confiscate there card and prevent them from accessing the system.</p>
+			<p>The program is a simplified banking system.  It allows users to log into the system using their card PIN.  It logs all transactions to a relational database, Microsoft Access, it also manages users failed login attempts and if nessesory confiscate there card and prevent them from accessing the system.</p>
 			
-			<p><a href="files/portfolio/DatabaseDevelopment.zip">PHP/MySQL Driven Website</a> - This contains a report about web database technologies, service side scripting and databases.  The main focus of these files is the PHP.  The PHP files are simple scripts that run diffrent SQL commands on the MySQL databse.  They add, remove and view data within the database.  The report shows the code in action as well as an entity relationship diagram of the system.</p> 
+			<p><a href="files/portfolio/DatabaseDevelopment.zip">PHP/MySQL Driven Website</a> - This contains a report about web database technologies, service side scripting and databases.  The main focus of these files is the PHP.  The PHP files are simple scripts that run different SQL commands on the MySQL database.  They add, remove and view data within the database.  The report shows the code in action as well as an entity relationship diagram of the system.</p> 
 			
 			<h2>Informational Reports</h2>
 			
 			<h3>Computer Science BSc Hon</h3>
-			<p><a href="files/portfolio/Network-Analasis-Report.pdf">Network Analysis Report</a> -  This report contains my recommendations regarding a fictional Universitys ICT services for a new building.  It covers what might be required for research laboratories, coffee shops and administration offices.</p>
+			<p><a href="files/portfolio/Network-Analasis-Report.pdf">Network Analysis Report</a> -  This report contains my recommendations regarding a fictional University's ICT services for a new building.  It covers what might be required for research laboratories, coffee shops and administration offices.</p>
 			
-			<p><a href="/files/portfolio/Mobile_Embedded_And_Wearable_Technology.pdf">Mobile, Embedded and Wearable Technology</a> - This report covers the design of a custom built device used to monitor <a href="http://en.wikipedia.org/wiki/Cribbing_%28horse%29" target="_BLANK">Cribing</a> in horses. It covers interconenction between devices (Microcontroller, acelorometer and real tiem clock) such as Ineter-Intergrated Circuit (I2C) and Serial Peropheral Interface (SPI) bus. Please see the assignmnet <a href="files/portfolio/Mobile_Embedded_And_Wearable_Technology_Specification.pdf">specification</a></p>
+			<p><a href="/files/portfolio/Mobile_Embedded_And_Wearable_Technology.pdf">Mobile, Embedded and Wearable Technology</a> - This report covers the design of a custom built device used to monitor <a href="http://en.wikipedia.org/wiki/Cribbing_%28horse%29" target="_BLANK">Cribing</a> in horses. It covers interconnection between devices (Microcontroller, accelerometer and real time clock) such as Ineter-Intergrated Circuit (I2C) and Serial Peripheral Interface (SPI) bus. Please see the assignment <a href="files/portfolio/Mobile_Embedded_And_Wearable_Technology_Specification.pdf">specification</a></p>
 
-			<p><a href="files/portfolio/Modelling_Persistent_Data.pdf">Modeling Persistent Data</a> - This is a small report detailing the modeling of persistent data using PostgreSQL. Including UML and SQL examples. Please see assignmnet <a href="files/portfolio/Modelling_Persistent_Data_Specification.pdf">specification</a> </p>
+			<p><a href="files/portfolio/Modelling_Persistent_Data.pdf">Modeling Persistent Data</a> - This is a small report detailing the modeling of persistent data using PostgreSQL. Including UML and SQL examples. Please see assignment <a href="files/portfolio/Modelling_Persistent_Data_Specification.pdf">specification</a> </p>
 
-			<p><a href="files/portfolio/Website_Usability.pdf">Website Usability</a> - This is a small report reviewing a few commercial websites, and one of my own design, regarding the sites usability and user interaction. Plase see assignmnet <a href="files/portfolio/Website_Usability_Specification.pdf">specifications</a></p>
+			<p><a href="files/portfolio/Website_Usability.pdf">Website Usability</a> - This is a small report reviewing a few commercial websites, and one of my own design, regarding the sites usability and user interaction. Please see assignment <a href="files/portfolio/Website_Usability_Specification.pdf">specifications</a></p>
 
 			<h3>HND Computing</h3>
-			<p>Here is a selection of assignments that I completed during my HND in Computing.  Some of the documents may be a bit confusing as they were written for someone with knowledge of the assignmnet specification.  Saying that they still contain some useful information, if not a little out of date.</p>
-			<p><a href="files/portfolio/NetworkDesignDiskAnalysis.pdf">Network Design and Disk Analysis</a> - This report is split into two sections.  The first being network design, this covers network topology, IP addressing, subnetting and network services.  The second section covers directory encoding using FAT12 and cluster accessing.  The second part is based on an assignment question which is not available, this makes it a little hard to follow.</p>
+			<p>Here is a selection of assignments that I completed during my HND in Computing.  Some of the documents may be a bit confusing as they were written for someone with knowledge of the assignment specification.  Saying that they still contain some useful information, if not a little out of date.</p>
+			<p><a href="files/portfolio/NetworkDesignDiskAnalysis.pdf">Network Design and Disk Analysis</a> - This report is split into two sections.  The first being network design, this covers network topology, IP addressing, sub-netting and network services.  The second section covers directory encoding using FAT12 and cluster accessing.  The second part is based on an assignment question which is not available, this makes it a little hard to follow.</p>
 			<p><a href="files/portfolio/RichInternetApplication.pdf">Rich Internet Application Development</a> - This report covers what is rich internet applications is and some fundamental reasons for using internet applications, as well as some examples of what technologies that are used.</p>
 			
 			
 			<h2>Web Design</h2>
-			<p>All of my clients have been local businesses who were in need of a simple site to get their company on the internet.  Alot of the work has been done using <a href="http://wordpress.com" target="_BLANK">Wordpress</a> as I find that it is easy for the clients to manage the site once I have given control to them.  Another reason for using wordpress is that it has an exstenable range of plugins that can be used to increase the functionality of the site.  It is also easy to create a good looking site from the ground up using wordpress functions.</p>
+			<p>All of my clients have been local businesses who were in need of a simple site to get their company on the internet.  A lot of the work has been done using <a href="http://wordpress.com" target="_BLANK">Wordpress</a> as I find that it is easy for the clients to manage the site once I have given control to them.  Another reason for using wordpress is that it has an extensible range of plugins that can be used to increase the functionality of the site.  It is also easy to create a good looking site from the ground up using wordpress functions.</p>
 			<p><h3>Priory Vets Cardigan</h3>
 			<a href="images/pages/portfolio/prioryvets-large.jpg" rel="shadowbox"><img alt="PrioryVets" border=0 width=700 height=150 src="images/pages/portfolio/prioryvets-small.jpg"/></a><a class="right" href="http://www.prioryvetscardigan.co.uk" target="_BLANK">www.prioryvetscardigan.co.uk</a> 
 			<div class="clear"></div></p>
@@ -44,17 +44,17 @@
 			<a class="right" href="http://www.razorbillribs.co.uk" target="_BLANK">www.razorbillribs.co.uk</a>
 			<div class="clear"></div>
 			</p>
-			<p>This site was created for a local rib charting comapny called Razor Bill Ribs.  They work from the coast of North Pembrokeshire/Ceredigion.  The site runs on wordpress with a heavaly modified theme to suite their needs.  The site has an extensive collection of images from the surrounding area.</p>
+			<p>This site was created for a local rib charting company called Razor Bill Ribs.  They work from the coast of North Pembrokeshire/Ceredigion.  The site runs on wordpress with a heavily modified theme to suite their needs.  The site has an extensive collection of images from the surrounding area.</p>
 			
 			<p><h3>Landsker Archaeology</h3>
 			<a href="images/pages/portfolio/landsker-large.jpg" rel="shadowbox"><img alt="Landsker" border=0 width=700 height=150 src="images/pages/portfolio/landsker-small.jpg"/></a><a class="right" href="http://www.landskerarchaeology.co.uk/" target="_BLANK">www.landskerarchaeology.co.uk</a><div class="clear"></div></p>
-			<p>Landsker archaeology is a local archaeological consultancy company supplying services to varied clients.  The site has general information about the business as well as links to usefull sites.  There is content to download as well as intergrated maps detailing geographic experience. It is running wordpress with a simple looking theme </p>
+			<p>Landsker archeology is a local archaeological consultancy company supplying services to varied clients.  The site has general information about the business as well as links to useful sites.  There is content to download as well as integrated maps detailing geographic experience. It is running wordpress with a simple looking theme </p>
 			
 			<p><h3>Anime For The Win</h3>
 			<a href="images/pages/portfolio/animeftw-large.jpg" rel="shadowbox"><img alt="Anime FTW" border=0 width=700 height=150 src="images/pages/portfolio/animeftw-small.jpg"/></a><a class="right" href="http://users.aber.ac.uk/pem9/cs15020/" target="_BLANK">users.aber.ac.uk/pem9/cs15020</a><div class="clear"></div></p>
-			<p>Created for CS15010.  Written from the ground up for an assignmnet.  The site uses ECMMA Script, Javascript, for form validation and message alerts.  The site conforms with XHTML 1.1 and CSS 2.1.</p>
+			<p>Created for CS15010.  Written from the ground up for an assignment.  The site uses ECMMA Script, Javascript, for form validation and message alerts.  The site conforms with XHTML 1.1 and CSS 2.1.</p>
 			
 			<p><h3>Nation Pigeon</h3>
 			<a href="images/pages/portfolio/nationpigeon-large.jpg" rel="shadowbox"><img alt="NationPigeon" border=0 width=700 height=150 src="images/pages/portfolio/nationpigeon-small.jpg"/></a><a class="right" href="http://nationpigeon.com" targt="_BLANK">www.nationpigeon.com</a><div class="clear"></div></p>
-			<p>The site contains my blog, which covers topics such as programming, web design, Unix administration and other random information.  It has been a working project since 2006 and has evoulved over the years to be a usefull source of information.  The site runs on the latest version of wordpress and uses a heavaly modified theme.</p>
+			<p>The site contains my blog, which covers topics such as programming, web design, Unix administration and other random information.  It has been a working project since 2006 and has evolved over the years to be a useful source of information.  The site runs on the latest version of wordpress and uses a heavily modified theme.</p>
 			<p>The site used to be hosted by streamline.  It is now run on my Debian based, Virtual Private Server thanks to <a href="http://allsimple.net" target="_BLANK">allsimple.net</a></p>
