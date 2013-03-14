656fc112b3804f97c085e477e00c39b457af4c7c
Pete Maynard <pgm987@gmail.com>
2013-03-14 19:01:06 +0000

    Moved image

--- home.php
+++ home.php
@@ -1,8 +1,9 @@
 <h2>About Me</h2>
-<img src="http://port22.co.uk/i/Osaka.jpg" alt="Image of Pete" class="left" />
 <p>Hello, I'm Pete! I'm a computer scientist, currently studying in Aberystwyth UK.  I have a keen interest in computer networks and programming.  I enjoy network security and server administration.</p>
 
 <p>Feel free to add me on any of the social networks above, whilst your here have a look at my <a href="index.php?p=blog">blog</a>, it might have 
 something interesting.  Also make sure to check out my <a href="index.php?p=cv">CV</a> and <a href="index.php?p=portfolio">Portfolio</a>.</p>
 
 <p>Thank you for looking, Pete</p>
+ 
+<img src="http://port22.co.uk/i/Osaka.jpg" alt="Image of Pete" align="center" />
