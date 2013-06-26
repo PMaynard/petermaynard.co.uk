662c6c4d32ac039fd23ad1f60d1a229bd79ae574
Pete <pgm987@gmail.com>
2013-06-27 00:30:34 +0100

    Start from scratch\!

--- public_html/home.php
+++
@@ -1,9 +0,0 @@
-<h2>About Me</h2>
-<p>Hello, I'm Pete! I'm a computer scientist, currently studying in Aberystwyth UK.  I have a keen interest in computer networks and programming.  I enjoy network security and server administration.</p>
-
-<p>Feel free to add me on any of the social networks above, whilst your here have a look at my <a href="index.php?p=blog">blog</a>, it might have 
-something interesting.  Also make sure to check out my <a href="index.php?p=cv">CV</a> and <a href="index.php?p=portfolio">Portfolio</a>.</p>
-
-<p>Thank you for looking, Pete</p>
- 
-<img src="http://port22.co.uk/i/Osaka.jpg" alt="Image of Pete" align="center" />
